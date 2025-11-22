b3b8ae7d0ac360faa76f2f7dfd8b5ceda3772861
Hitakankshi Behera <hitakankshib@gmail.com>
2025-11-22 05:38:51 +0000

    W.A (_for element text_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -23,7 +23,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
         fill="url(#goldGradient)"
         fontWeight="bold"
       >
-        WLA
+        W.A
       </text>
     </svg>
   );
